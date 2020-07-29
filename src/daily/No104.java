b9df5e713e184acbbfa6abd3eb4dde9294c45f90
Camille <pengkangzaia@foxmail.com>
2020-07-29 09:21:05 +0800

    daily

---
+++ src/daily/No104.java
@@ -0,0 +1,13 @@
+package daily;
+
+
+public class No104 {
+    // 查找二叉树的最大深度
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+}
