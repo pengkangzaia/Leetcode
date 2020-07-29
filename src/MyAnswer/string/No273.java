ccbc20f9d05033acd000e3acb2facb3cbda917b6
Camille <pengkangzaia@foxmail.com>
2020-07-29 22:18:21 +0800

    string

---
+++ src/MyAnswer/string/No273.java
@@ -0,0 +1,31 @@
+package MyAnswer.string;
+
+public class No273 {
+
+    // int 范围 ： 2,147,483,647
+    // billion, million, thousand
+
+
+
+    public static String numberToWords(int num) {
+//        String s = String.valueOf(num);
+//        char[] chars = s.toCharArray();
+//        int i = chars.length - 1;
+//        int count = 0;
+//        StringBuilder res = new StringBuilder();
+//        StringBuilder tem = new StringBuilder();
+//        while (i >= 0) {
+//            if (count % 3 == 0) {
+//                res.append(tem);
+//                tem = new StringBuilder();
+//            }
+//
+//        }
+//        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberToWords(100));
+    }
+
+}
