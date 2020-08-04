e2970b544230bb451df68f28aa14f806dc8ae13d
Camille <pengkangzaia@foxmail.com>
2020-08-04 23:04:59 +0800

    每日一题

---
+++ src/MyAnswer/string/substring/No395.java
@@ -0,0 +1,20 @@
+package MyAnswer.string.substring;
+
+public class No395 {
+
+    public int longestSubstring(String s, int k) {
+
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        // HashMap<Character, Integer> map = new HashMap<>();
+        char[] chars = new char[26];
+        int l = 0, r = 0, ansL = 0, ansR = 0, maxLen = 0;
+        return 0;
+
+
+    }
+
+
+}
