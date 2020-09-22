84b8e838074029ea3c9c760d41da3ff5d08eac29
Camille <pengkangzaia@foxmail.com>
2020-09-22 23:01:46 +0800

    子集

--- src/MyAnswer/backtracking/No78.java
+++ src/MyAnswer/backtracking/No78.java
@@ -17,33 +17,28 @@ public class No78 {
 
     // 路径：所有遍历过的集合
     // 待选：没有完全遍历完的元素集合
-    // 结束：到达数组大小
+    // 结束：这题没有结束条件
 
     List<List<Integer>> res = new LinkedList<>();
-    boolean[] visit;
 
     public List<List<Integer>> subsets(int[] nums) {
         LinkedList<Integer> path = new LinkedList<>();
-        visit = new boolean[nums.length];
-        traverse(nums, path);
+        traverse(nums, path, 0);
         return res;
     }
 
-    public void traverse(int[] nums, LinkedList<Integer> path) {
+    public void traverse(int[] nums, LinkedList<Integer> path, int index) {
         // 不能直接加path啊
         res.add(new LinkedList<>(path));
-        if (path.size() == nums.length) {
+        // 这题没有结束条件？其实是有的，不过隐含在循环中了。
+        if (index > nums.length) {
             return;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (visit[i]) {
-                continue;
-            }
+        for (int i = index; i < nums.length; i++) {
             path.add(nums[i]);
-            visit[i] = true;
-            traverse(nums, path);
-//            visit[i] = false;
-            path.removeLast();
+            // 注意这里时 i+1，不是 index+1，index在这里是不变的
+            traverse(nums, path, i + 1);
+            path.remove(path.size() - 1);
         }
     }
 
