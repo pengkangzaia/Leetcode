8bd9af7f03c7a0760195a0f5880243dd0d72baf4
kang.peng <kang.peng@dingxiang-inc.com>
2020-09-24 10:04:18 +0800

    下一个排列(数组解法)

---
+++ src/MyAnswer/backtracking/No31.java
@@ -0,0 +1,44 @@
+package MyAnswer.backtracking;
+
+import java.util.Arrays;
+
+/**
+ * @FileName: No31.java
+ * @Description: No31.java类说明
+ * @Author: 彭康
+ * @Date: 2020/9/24 9:48
+ */
+public class No31 {
+
+    public void nextPermutation(int[] nums) {
+        for (int i = nums.length - 1; i >= 1; i--) {
+            if (nums[i - 1] < nums[i]) {
+                for (int j = nums.length - 1; j >= i; j--) {
+                    if (nums[i - 1] < nums[j]) {
+                        swap(nums, i - 1, j);
+                        Arrays.sort(nums, i, nums.length);
+                        return;
+                    }
+                }
+            }
+        }
+        Arrays.sort(nums);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+    public static void main(String[] args) {
+        No31 no31 = new No31();
+        int[] nums = {1,5,2,3,0,0,2};
+        no31.nextPermutation(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+}
