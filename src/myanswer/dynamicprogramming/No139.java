aed2b7a22e3385fd61c57ebc444dff81db40ba72
Camille <pengkangzaia@foxmail.com>
2020-10-10 00:21:26 +0800

    dp-单词拆分

--- src/myanswer/dynamicprogramming/No139.java
+++ src/myanswer/dynamicprogramming/No139.java
@@ -1,5 +1,6 @@
 package myanswer.dynamicprogramming;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -11,8 +12,34 @@ import java.util.List;
 public class No139 {
 
     public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+        int maxLength = 0;
+        for (int i = 0; i < wordDict.size(); i++) {
+            maxLength = Math.max(wordDict.get(i).length(), maxLength);
+        }
+        for (int i = 1; i <= n; i++) {
+            // 优化1：只需要查看wordDict中的最长字符串长度的范围即可
+            // 注意：j需要从后往前数，因为j=0,i=5时，j+4>=5不满足，会直接跳出循环
+            for (int j = i - 1; j >= 0 && j + maxLength >= i; j--) {
+                if (wordDict.contains(s.substring(j, i)) && dp[j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
 
-        return false;
+    public static void main(String[] args) {
+        No139 no139 = new No139();
+        String s = "leetcode";
+        List<String> wordDict = new LinkedList<>();
+        wordDict.add("leet");
+        wordDict.add("code");
+        boolean b = no139.wordBreak(s, wordDict);
+        System.out.println(b);
     }
 
 }
