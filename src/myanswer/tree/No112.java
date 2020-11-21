2e576deff7e7e4b53f5b2be4499212eed04c7eb3
peng kang <pengkangzaia@foxmail.com>
2020-11-21 22:12:39 +0800

    TOP interview

--- src/myanswer/tree/No112.java
+++ src/myanswer/tree/No112.java
@@ -19,6 +19,7 @@ public class No112 {
         if (root.left == null && root.right == null) {
             return sum == root.val;
         }
+
         sum -= root.val;
         return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
 
