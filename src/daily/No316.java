df37d13d9bd09e60a313bf994b35ca932bbd67c9
Camille <pengkangzaia@foxmail.com>
2020-12-21 17:11:11 +0800

    去除重复字母

--- src/daily/No316.java
+++ src/daily/No316.java
@@ -5,40 +5,38 @@ import java.util.Stack;
 /**
  * @FileName: No316.java
  * @Description: 去除重复字母
- * @Author: camille
- * @Date: 2020/12/20 12:56
+ * @Author: admin
+ * @Date: 2020/12/21 11:36
  */
 public class No316 {
 
     public String removeDuplicateLetters(String s) {
         Stack<Character> stack = new Stack<>();
         int n = s.length();
-        int i = n - 1;
-        while (i >= 0) {
+        for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
-            i--;
-            if (stack.isEmpty()) {
-                stack.push(c);
-            } else if (!stack.contains(c)) {
-                stack.push(c);
-            } else if (c >= stack.peek()) {
-                continue;
-            } else {
-                int search = stack.search(c);
-                stack.remove(stack.size() - search);
+            if (!stack.contains(c)) {
+                if (stack.isEmpty() || stack.peek() < c) {
+                    stack.push(c);
+                    continue;
+                }
+                while (!stack.isEmpty() && stack.peek() > c && s.indexOf(stack.peek(), i) != -1) {
+                    stack.pop();
+                }
                 stack.push(c);
             }
         }
-        StringBuilder sb = new StringBuilder();
+        StringBuilder res = new StringBuilder();
         while (!stack.isEmpty()) {
-            sb.append(stack.pop());
+            res.append(stack.pop());
         }
-        return sb.toString();
+        return res.reverse().toString();
     }
+    // 栈顶 > 当前 z>b  zbcb   z
 
     public static void main(String[] args) {
         No316 no316 = new No316();
-        String ans = no316.removeDuplicateLetters("cbacdcbc");
+        String ans = no316.removeDuplicateLetters("zcbcb");
         System.out.println(ans);
     }
 
