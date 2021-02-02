6de96c9d265b42ffe63dbc019c80fb2da8546ccb
Camille <pengkangzaia@foxmail.com>
2021-02-02 11:28:21 +0800

    No48 旋转图像 先对角线交换然后水平交换

--- src/myanswer/array/medium/No48.java
+++ src/myanswer/array/medium/No48.java
@@ -1,5 +1,5 @@
 package myanswer.array.medium;
-
+// no48 旋转图像
 public class No48 {
 
     public void rotate(int[][] matrix) {
@@ -24,6 +24,28 @@ public class No48 {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
+        }
+    }
+
+    public void rotate1(int[][] matrix) {
+        // 先对角线翻转，然后对每一行（一维矩阵）进行首尾翻转
+        int m = matrix.length;
+        for (int i = 0; i < m; i++) {
+            for (int j = i + 1; j < m; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            int j = 0, k = m - 1;
+            while (j <= k) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = temp;
+                j++;
+                k--;
+            }
         }
     }
 
