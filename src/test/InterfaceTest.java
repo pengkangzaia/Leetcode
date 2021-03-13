b9f1b1a9b072ca7745a0df412c6305506e821365
peng kang <pengkangzaia@foxmail.com>
2021-03-14 01:18:19 +0800

    接口特性测试

---
+++ src/test/InterfaceTest.java
@@ -0,0 +1,19 @@
+package test;
+
+/**
+ * @FileName: InterfaceTest.java
+ * @Description: InterfaceTest.java类说明
+ * @Author: camille
+ * @Date: 2021/3/13 20:34
+ */
+public interface InterfaceTest {
+
+    // 接口中的方法只能是public abstract的
+
+    public abstract void open();
+
+    public void close();
+
+    abstract void open1();
+
+}
