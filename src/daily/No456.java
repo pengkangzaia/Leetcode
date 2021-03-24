9a2976bc458b8ca9f47dce2f92572d6a9d1bcbbe
Camille <pengkangzaia@foxmail.com>
2021-03-24 17:41:21 +0800

    移除无效注释

--- src/daily/No456.java
+++ src/daily/No456.java
@@ -1,9 +1,7 @@
 package daily;
 
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
-import java.util.Stack;
 
 /**
  * @FileName: No456.java
@@ -61,33 +59,9 @@ public class No456 {
         // [-2,1,2,-2,1,2]
         // 最小值数组+从后往前遍历获取单调递增栈
         System.out.println(no456.find132pattern(new int[]{2, 3, 1, 2}));
-        /*System.out.println(no456.find132pattern(new int[]{-1, 3, 2, 0}));
-        System.out.println(no456.find132pattern(new int[]{1, 2, 3, 4}));*/
-
     }
 
-    /**
-     *         int n = nums.length;
-     *         int[] min = new int[n];
-     *         min[0] = nums[0];   // 第一个位置的最小数肯定就是它自己了
-     *         // 将前一个位置的最小数和当前位置的数比较，找到当前位置的最小数
-     *         for (int i = 1; i < n; ++i) min[i] = Math.min(min[i - 1], nums[i]);
-     *         Stack<Integer> stack = new Stack<>();
-     *         // 从后往前遍历，stack 中的数字表示的是从位置 i 到 n 中，大于 min[i] 且小于 nums[i] 的数
-     *         for (int i = n - 1; i >= 0; --i) {
-     *             if (nums[i] > min[i]) {
-     *                 // 如果栈中的数字比 min[i] 还小或者相同，那么说明不可能是 ak，就弹出来
-     *                 while (!stack.isEmpty() && stack.peek() <= min[i]) stack.pop();
-     *                 // 检查一下栈顶元素是不是满足 ai<stack.peek()<aj，如果满足就说明找到了
-     *                 if (!stack.isEmpty() && stack.peek() < nums[i]) return true;
-     *                 // 不管怎样都要push进来当前的数，因为当前的数满足了大于 min[i]
-     *                 stack.push(nums[i]);
-     *             }
-     *         }
-     *         // 到最后都没找到，肯定只能返回false了
-     *         return false;
-     *
-     */
+
 
 
 }
